45134354ac06bd552bc6904f3ace1663f242460b
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-24 23:18:12 +0530

    Logging Demo

--- src/main/java/com/chetan/springwebapptodo/login/LoginController.java
+++ src/main/java/com/chetan/springwebapptodo/login/LoginController.java
@@ -1,17 +1,23 @@
 package com.chetan.springwebapptodo.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 public class LoginController {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("/login")
-    public String showLoginPage(@RequestParam String name, ModelMap model) {
-        //model gives the data to the view, model is provided by spring and its a map
-        model.put("name", name);
+    public String showLoginPage(@RequestParam Optional<String> name, ModelMap model) {
+        //model gives the data to the view, model is provided by spring, and it's a map
+        logger.info("Request param: name is {}", name.orElse("Guest"));
+        model.put("name", name.orElse("Guest"));
         return "login";
     }
 }
