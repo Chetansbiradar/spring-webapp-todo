394d3caad4509325f12380d48aa00c1b9467a81a
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-27 00:07:59 +0530

    + H2 DB Spring Security Config to make H2 console work

--- src/main/java/com/chetan/springwebapptodo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/chetan/springwebapptodo/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.chetan.springwebapptodo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -34,5 +37,25 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //By default, Spring Security
+    //Protects all the urls
+    //Adds login form for unauthenticated users
+
+    //But H2 database need
+    // CSRF disabled
+    // Frames allowed
+
+    // So we need to override the default behaviour of Spring Security
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                req->req.anyRequest().authenticated()
+        );
+        http.formLogin(Customizer.withDefaults());
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
 }
